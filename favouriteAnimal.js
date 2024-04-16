047d76f3494a91ee69dec252205a1e5a3e4d53e1
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-04-16 20:01:17 +1000

    feat: animals have a remove button

--- favouriteAnimal.js
+++ favouriteAnimal.js
@@ -20,10 +20,43 @@ function createAnimalList(){
         newList.textContent = animal;
         newList.id = animal;
 
+        // 1b. Add a button to remove the element from the list
+        let removeItemButton = document.createElement("button");
+
+        removeItemButton.onclick = () => removeAnimalFromList(animal);
+
+        removeItemButton.textContent = "Remove Animal";
+
+        newList.appendChild(removeItemButton);
+
         //2. Find the ol element that exists in the page and append the li into it
         let root01Node = document.querySelector("ol");
         root01Node.appendChild(newList);
 
     });
    
+}
+
+function removeAnimalFromList(targetAnimalID){
+    //1. Find element in list with matching id
+    let targetListItem = document.getElementById(targetAnimalID);
+    targetListItem.remove();
+
+    //2. Check if ID is in array of animals
+    let isAnimalInList = animals.includes(targetAnimalID);
+    if (isAnimalInList){
+    } else {
+        return;
+    }
+
+    //3. Remove the animal from array
+    animals = animals.filter(animal  => {
+        if (targetAnimalID == animal){
+            return false;
+        } else {
+            return true;
+        }
+    });
+
+    //4. Update or wipe & rebuild the animal list HTML
 }
