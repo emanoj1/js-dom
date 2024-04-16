451cd1600f8b49b3f75c5e38aafb45e3e41c9c8b
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-04-16 20:49:10 +1000

    feat: implemented CSS for light & dark schemes

---
+++ themeControl.js
@@ -0,0 +1,20 @@
+const themes = {
+    light:{
+        background: "white",
+        color: "darkgrey"
+    },
+    dark:{
+        background: "darkgrey",
+        color: "#000000"
+    }
+};
+
+function changeCssTheme(themeName){
+    // themes[themeName].variable
+    for (const variable in themes[themeName]){
+        document.documentElement.style.setProperty(`--${variable}`, themes[themeName][variable]);
+        console.log("Updated CSS variable --" + variable);
+    }
+}
+
+changeCssTheme(dark);
